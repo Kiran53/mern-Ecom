838577ccef5a2f0ae855cc8839f173238c2d7602
Kiran53 <56129777+Kiran53@users.noreply.github.com>
2022-07-11 22:01:53 +0530

    no idea

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express, { json } from 'express';
+import { connect } from 'mongoose';
 // const path = require('path');
-const config = require('config');
+import { util } from 'config';
 require('dotenv').config()
-const authRoutes = require('./routes/auth');
-const itemRoutes = require('./routes/item');
-const cartRoutes = require('./routes/cart');
-const orderRoutes = require('./routes/order');
-const cookieParser = require('cookie-parser')
-const path = require("path");
+import authRoutes from './routes/auth';
+import itemRoutes from './routes/item';
+import cartRoutes from './routes/cart';
+import orderRoutes from './routes/order';
+import cookieParser from 'cookie-parser';
+import path from "path";
 const app = express();
-app.use(express.json())
+app.use(json())
 app.use(cookieParser())
-
 
 app.get('/',(req, res)=> console.log("here"))
 app.use('/api',authRoutes);
@@ -21,18 +20,20 @@ app.use('/api',cartRoutes);
 app.use('/api',orderRoutes);
 
 // if(process.env.NODE_ENV === 'production' || 1) {
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     });
-// }
-// app.use(express.static(path.join(__dirname, '/client/build')))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build'))
-// })
-// const dbURI = config.get('dbURI');
-const dbURI = `mongodb+srv://KiranEkom:Oneto34five@cluster0.w8jmi.mongodb.net/?retryWrites=true&w=majority`
-const port = process.env.PORT || 4000;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then(() => app.listen(port, () => console.log(`Server running on http://localhost:${port}`)))
-  .catch((err) => console.log(err));
+  //     app.use(express.static('client/build'));
+  //     app.get('*', (req, res) => {
+    //       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    //     });
+    // }
+    // app.use(express.static(path.join(__dirname, '/client/build')))
+    // app.get('*', (req, res) => {
+      //     res.sendFile(path.join(__dirname, '/client/build'))
+      // })
+      // const dbURI = config.get('dbURI');
+      const dbURI = `mongodb+srv://KiranEkom:Oneto34five@cluster0.w8jmi.mongodb.net/?retryWrites=true&w=majority`
+      const port = process.env.PORT || 4000;
+      connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+      .then(() => app.listen(port, () => console.log(`Server running on http://localhost:${port}`+ util.getEnv('NODE_ENV') )))
+      .catch((err) => console.log(err));
+      
+      
